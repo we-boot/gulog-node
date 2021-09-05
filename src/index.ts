da90a4dedd8cf43490d39cf66ef4d2be64ff7522
Stijn Rogiest <reddusted@gmail.com>
2021-09-05 12:45:25 +0200

    Log functions & update gulog-js

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { AsyncLocalStorage } from "async_hooks";
-import { GulogProcess, GulogSettings, InitiatorData } from "gulog-js";
+import Gulog from "gulog-js";
 
-let store = new AsyncLocalStorage<GulogProcess>();
+let store = new AsyncLocalStorage<Gulog.Process>();
 
 /**
  * Creates a new Gulog process with async context, which you can log to using warn, error, info ...
@@ -16,20 +16,54 @@ let store = new AsyncLocalStorage<GulogProcess>();
  * @param callback Every call to log, info, warn, error ... in this function will log to this newly created process.
  */
 export function withGulog<T>(type: string, callback: () => T): T;
-export function withGulog<T>(type: string, initiatorData: InitiatorData, callback: () => T): T;
-export function withGulog<T>(type: string, initiatorData: InitiatorData, overrideSettings: GulogSettings, callback: () => T): T;
+export function withGulog<T>(type: string, initiatorData: Gulog.InitiatorData, callback: () => T): T;
+export function withGulog<T>(type: string, initiatorData: Gulog.InitiatorData, overrideSettings: Gulog.Settings, callback: () => T): T;
 export function withGulog<T>(type: string, arg0: any, arg1?: any, arg2?: any): T {
     let parentProcess = store.getStore();
     if (typeof arg0 === "function") {
-        let process = new GulogProcess(type, undefined, parentProcess);
+        let process = new Gulog.Process(type, undefined, parentProcess);
         return store.run(process, arg0);
     } else if (typeof arg1 === "function") {
-        let process = new GulogProcess(type, arg0, parentProcess);
+        let process = new Gulog.Process(type, arg0, parentProcess);
         return store.run(process, arg1);
     } else {
-        let process = new GulogProcess(type, arg0, parentProcess, arg1);
+        let process = new Gulog.Process(type, arg0, parentProcess, arg1);
         return store.run(process, arg2);
     }
+}
+
+export function getCurrentProcess() {
+    let p = store.getStore();
+    if (!p) throw new Error("There is no running Gulog process.");
+    return p;
+}
+
+export function log(data: any, ...moreData: any[]) {
+    let p = getCurrentProcess();
+    return p.log(data, ...moreData);
+}
+
+export function warn(data: any, ...moreData: any[]) {
+    let p = getCurrentProcess();
+    return p.warn(data, ...moreData);
+}
+
+export function info(data: any, ...moreData: any[]) {
+    let p = getCurrentProcess();
+    return p.info(data, ...moreData);
+}
+
+export function error(data: any, ...moreData: any[]) {
+    let p = getCurrentProcess();
+    return p.error(data, ...moreData);
+}
+
+/**
+ * @param exitCode An exit code describing the process exit cause, examples: `user-create-failed`, `failed`, `ok` or its number id.
+ */
+export function end(exitCode: string | number) {
+    let p = getCurrentProcess();
+    return p.end(exitCode);
 }
 
 export * from "gulog-js";
